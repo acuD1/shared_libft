9a5f79de827a285103ba52913d9c43c766a2a2df
Guillaume VILLAT <guvillat@e1r4p13.42.fr>
2020-03-11 21:48:26 +0100

    arthur les heredocs ca marche bien quand tu touches pas a la shared SINON JE VEUX ALLER AU RAMPAGE LA PUTAIN FUCK LE CORONA

--- srcs/ft_strlen_to.c
+++ srcs/ft_strlen_to.c
@@ -6,7 +6,7 @@
 /*   By: arsciand <arsciand@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/08 11:41:04 by fcatusse          #+#    #+#             */
-/*   Updated: 2020/03/04 23:09:38 by arsciand         ###   ########.fr       */
+/*   Updated: 2020/03/11 21:47:05 by guvillat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,11 @@ size_t	ft_strlen_to(const char *s, char c)
 	size_t	i;
 
 	i = 0;
-	while (s[i])
+	while (s && s[i])
 	{
 		if (s[i] == c)
 			return (i);
+		i++;
 	}
 	return (i);
 }
